9cf85747294356cd2818b7b51da1aef12ea5df6a
Cauchy2b <841907997@qq.com>
2024-07-14 15:58:32 +0800

    Bus Params Added

---
+++ chisel-proj/src/main/scala/bus/ultra/UltraBusParams.scala
@@ -0,0 +1,31 @@
+package bus.ultra
+import chisel3._
+object UltraBusParams {
+  val wordLength = 32
+  val iWords = 8
+  val dWords = 4
+  val iBandWidth = iWords * wordLength
+  val dBandWidth = dWords * wordLength
+  val _M = math.pow(10,6).toInt
+  val cpuFrequency = 60 * _M
+  val sramFrequency = 60 * _M
+  val memCycles = math.ceil(cpuFrequency / sramFrequency)
+
+  // Sram Virtual Addr(31,22)
+  val baseSramAddr = "b1000_0000_00".U
+  val extSramAddr = "b1000_0000_01".U
+  // Kernel Working Space and User Working Space
+  // Program Addr: Virtual Addr(21,20)
+  val coreProgAddr = "b00".U
+  val usrProgAddr = "b01".U // 01,10,11 are all user program space
+  // Data Addr: Virtual Addr(21,16)
+  val usrDataAddr = "b00_0000".U
+  val coreDataAddr = "b11_1111".U
+
+  // Uart Virtual Addr
+  // For simplicity,Check (31,24) is enough
+  val uartAddr = "hbf".U
+  // Status and Data Addr:Virtual Addr(3,0)
+  val uartStatAddr = "hc".U
+  val uartDataAddr = "h8".U
+}

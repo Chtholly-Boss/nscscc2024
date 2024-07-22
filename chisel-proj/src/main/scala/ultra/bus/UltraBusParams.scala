f4afee3c036eb830fe5f935d823d4cc688284457
Cauchy2b <841907997@qq.com>
2024-07-23 00:06:02 +0800

    200M stable version

--- chisel-proj/src/main/scala/ultra/bus/UltraBusParams.scala
+++ chisel-proj/src/main/scala/ultra/bus/UltraBusParams.scala
@@ -10,10 +10,10 @@ object UltraBusParams {
   val iBandWidth = iWords * wordLength
   val dBandWidth = dWords * wordLength
   val _M = math.pow(10,6).toInt
-  val cpuFrequency = 210 * _M
+  val cpuFrequency = 200 * _M
   val sramFrequency = 60 * _M
   val memCycles = 4
-  val mulCycles = 1
+  val mulCycles = 2
 
   val initPcAddr = "h8000_0000".U
   // Sram Virtual Addr(31,22)
